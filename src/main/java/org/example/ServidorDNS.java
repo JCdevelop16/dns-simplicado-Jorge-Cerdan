e23cf5af2100eebd5a1ec7c28b321db2e6450347
JCdevelop16 <jorgeicerdan16@gmail.com>
2025-11-27 13:36:01 +0100

    commmit y prueba Tag

--- src/main/java/org/example/ServidorDNS.java
+++ src/main/java/org/example/ServidorDNS.java
@@ -1,17 +1,15 @@
 package org.example;
 
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class Main {
+public class ServidorDNS {
     static void main() throws IOException {
-        HashMap<String, List<String>> listaMap = new HashMap();
-        List<String> listaServidores = new ArrayList();
+        HashMap<String, List<String>> listaMap = new HashMap<>();
+        List<String> listaServidores = new ArrayList<>();
 
         int puerto = 5000;
         try(ServerSocket serverSocket = new ServerSocket(puerto)) {
