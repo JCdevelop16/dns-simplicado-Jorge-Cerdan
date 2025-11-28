6de6306307b9c89fc2e90dfcf80bf52288641c2d
JCdevelop16 <jorgeicerdan16@gmail.com>
2025-11-28 10:07:18 +0100

    Claase registro corregida y añadimos valores al HashMap

---
+++ src/main/java/org/example/Registro.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.List;
+
+public class Registro {
+
+    String dominio;
+    String tipo;
+    String valor;
+
+
+    public Registro() {}
+
+    public Registro(String dominio, String tipo, String valor) {
+        this.dominio = dominio;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getDominio() {
+        return dominio;
+    }
+
+    public void setDominio(String dominio) {
+        this.dominio = dominio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
--- src/main/java/org/example/ServidorDNS.java
+++ src/main/java/org/example/ServidorDNS.java
@@ -1,19 +1,41 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class ServidorDNS {
+
+    static HashMap<String, List<Registro>> listaMap;
+
     static void main() throws IOException {
-        HashMap<String, List<String>> listaMap = new HashMap<>();
-        List<String> listaServidores = new ArrayList<>();
+
+        HashMap<String, List<Registro>> diccionario = new HashMap<String, List<Registro>>();
+
+        try {
+            File direcciones = new File("src/direcciones.txt");
+            BufferedReader br = new BufferedReader(new FileReader(direcciones));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] partes = line.split(" ");
+                if(partes.length <= 3){
+                    Registro registro = new Registro(partes[0], partes[1], partes[2]);
+                    if(!diccionario.containsKey(partes[0])){
+                        diccionario.put(partes[0], new ArrayList<>());
+                        diccionario.get(partes[0]).add(registro);
+                    }else{
+                        diccionario.get(partes[0]).add(registro);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("No se encuentra el direcciones.txt");
+        }
+
 
         int puerto = 5000;
         try(ServerSocket servidor = new ServerSocket(puerto)) {
@@ -25,6 +47,11 @@ public class ServidorDNS {
             PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
 
 
+
+            // Cierre
+            System.out.println("Conexión cerrada con el cliente.");
+            cliente.close();
+            System.exit(0);
 
         }
 
